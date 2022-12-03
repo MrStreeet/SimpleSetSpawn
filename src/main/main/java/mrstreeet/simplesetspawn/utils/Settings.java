a80a91971fd0f667cee1ee61a0161a72262d9d1f
MrStreeet <50084238+MrStreeet@users.noreply.github.com>
2022-12-03 01:35:36 +0100

    Delete src/main/main directory

--- src/main/main/java/mrstreeet/simplesetspawn/utils/Settings.java
+++
@@ -1,8 +0,0 @@
-package mrstreeet.simplesetspawn.utils;
-
-public class Settings {
-
-    public static String RESOURCE_LINK = "https://bit.ly/3H1loRI";
-    public static  String VERSION = "1.0.1-BETA";
-
-}
