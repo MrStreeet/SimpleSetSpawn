34c12cd97fe48bf7125f02d87a1e4b71713f0927
MrStreeet <50084238+MrStreeet@users.noreply.github.com>
2022-12-03 01:38:25 +0100

    Add files via upload

--- src/main/java/mrstreeet/simplesetspawn/SimpleSetSpawn.java
+++ src/main/java/mrstreeet/simplesetspawn/SimpleSetSpawn.java
@@ -4,13 +4,18 @@ import mrstreeet.simplesetspawn.commands.MainCommand;
 import mrstreeet.simplesetspawn.commands.SetSpawn;
 import mrstreeet.simplesetspawn.commands.Spawn;
 import mrstreeet.simplesetspawn.events.AlwaysTP;
+import mrstreeet.simplesetspawn.events.Update;
+import mrstreeet.simplesetspawn.utils.UpdateChecker;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 public final class SimpleSetSpawn extends JavaPlugin {
 
@@ -18,19 +23,26 @@ public final class SimpleSetSpawn extends JavaPlugin {
     public String version = pdffile.getVersion(); // Gets version of the plugin
     public String name = ChatColor.GOLD + "[" + pdffile.getName() + "]"; // Gets name of the plugin
 
-
     public String configPath;
 
+    public static SimpleSetSpawn plugin;
+
+    private static Plugin instance;
 
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage(name + ChatColor.GREEN + " It's now working correctly!.");
-        Bukkit.getConsoleSender().sendMessage(name + ChatColor.GREEN + " Do you like this plugin? Help me with a donation here!: " + ChatColor.WHITE + "ko-fi.com/mrstreeet");
+        Bukkit.getConsoleSender().sendMessage(name + ChatColor.YELLOW + " Plugin it's loading...");
+
+        plugin = this;
+        instance = this;
 
         registerCommand();
         registerEvents();
         saveDefaultConfig();
+        updateChecker();
 
+        Bukkit.getConsoleSender().sendMessage(name + ChatColor.GREEN + " It's now working correctly!.");
+        Bukkit.getConsoleSender().sendMessage(name + ChatColor.GREEN + " Do you like this plugin? Help me with a donation here!: " + ChatColor.WHITE + "ko-fi.com/mrstreeet");
     }
 
     @Override
@@ -47,6 +59,25 @@ public final class SimpleSetSpawn extends JavaPlugin {
     public void registerEvents(){
         PluginManager pm =getServer().getPluginManager();
         pm.registerEvents(new AlwaysTP(this), this);
+        pm.registerEvents(new Update(this), this);
+    }
+
+    public void updateChecker(){
+        new UpdateChecker(this, 106488).getLatestVersion(version -> {
+            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                Bukkit.getConsoleSender().sendMessage(name + ChatColor.GREEN + " It's up to date (Running version: " + ChatColor.WHITE +  version + ")");
+            }else{
+                Bukkit.getConsoleSender().sendMessage(name + ChatColor.RED + " You are running an old version of the plugin, please update to the latest version.");
+            }
+        });
+    }
+
+    public static SimpleSetSpawn getPlugin() {
+        return plugin;
+    }
+
+    public static Plugin getInstance() {
+        return instance;
     }
 
     //Creation of config.yml
